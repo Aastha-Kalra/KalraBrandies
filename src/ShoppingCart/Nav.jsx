3b2ca94d71a6c42ff323bc508ba38cf96c938d0d
Aastha-Kalra <126173305+Aastha-Kalra@users.noreply.github.com>
2024-02-09 17:36:43 +0530

    Update Nav.jsx

--- src/ShoppingCart/Nav.jsx
+++ src/ShoppingCart/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
   return (
     <div className="md:px-10 md:py-5 py-5 flex justify-around shadow-md sticky top-0 bg-white text-xl ">
   <h1 className="font-bold text-purple-600 animate-bounce">Kalra's Brandies</h1>
-      <div className="flex justify-end gap-24">
+      <div className="flex justify-end gap-10">
         <Link to={"/"}>Home</Link>
         <Link to={"/cart"}>
           My Bag: &nbsp;
