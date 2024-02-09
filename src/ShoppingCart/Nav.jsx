fd5db9b832942704d1857309251be4faba56b37c
Aastha-Kalra <126173305+Aastha-Kalra@users.noreply.github.com>
2024-02-09 17:35:06 +0530

    Update Nav.jsx

--- src/ShoppingCart/Nav.jsx
+++ src/ShoppingCart/Nav.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 const Nav = () => {
   const items = useSelector((state) => state.cart);
   return (
-    <div className="md:px-10 md:py-5 py-5 flex justify-around shadow-md sticky top-0 bg-white text-2xl ">
+    <div className="md:px-10 md:py-5 py-5 flex justify-around shadow-md sticky top-0 bg-white text-xl ">
   <h1 className="font-bold text-purple-600 animate-bounce">Kalra's Brandies</h1>
       <div className="flex justify-end gap-24">
         <Link to={"/"}>Home</Link>
